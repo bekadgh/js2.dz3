c53548b54a765b035d6e625799dd130259cb6206
Beksultan Sulaimanov <beksultansulaimanov77@gmail.com>
2022-10-17 17:27:21 +0600

    js2.dz3

---
+++ main.js
@@ -0,0 +1,27 @@
+//ЗАДАНИЕ 1
+// const inp = document.querySelector('input')
+// const btn = document.querySelector('#btn')
+// let age = 18
+
+// btn.addEventListener('click',() => {
+//     let num = new Promise((resolve,reject) => {
+//         if (inp.value == age) {
+//             resolve('Ура вы угадали')
+//         }else {
+//             reject('Увы вы не угадали, попробуйте еще раз')
+//         }
+//     }).then((resolve) => console.log(resolve))
+//     .catch((reject) => console.log(reject))
+// })
+//ЗАДАНИЕ 2
+// const arm = 'javaScript'
+// let jss = 10
+// btn.addEventListener('click',() => {
+//         let num = new Promise((resolve,reject) => {
+//             if (inp.value == jss) {
+//                 alert('Вы угадали')
+//             }else {
+//                 alert('Не правильно')
+//             }
+//         })
+// })
